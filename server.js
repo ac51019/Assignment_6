9b524028dfc6596996ee39e0c68698f773eaaa3c
Anshu Chauhan <ac5500119@gmail.com>
2026-05-15 15:03:32 +0530

    doneKey Points of the Server

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const http = require("http");
 const fs = require("fs"); // fs-file system module to read files from the server
 const path = require("path");
 
+
 const PORT = 3000;
+
 
 // Helper function to serve HTML files
 const serveFile = (filePath, res, statusCode = 200) => {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500);
-      res.end("Server Error");
+      res.writeHead(500); // Internal Server Error
+      res.end("Server Error");  // Handle file read errors
     } else {
       res.writeHead(statusCode, { "Content-Type": "text/html" });
       res.end(data);
@@ -17,15 +19,17 @@ const serveFile = (filePath, res, statusCode = 200) => {
   });
 };
 
+
 const server = http.createServer((req, res) => {
   const url = req.url;
 
-  if (url === "/" || url === "/index") {
-    serveFile(path.join(__dirname, "pages", "index.html"), res);
+
+  if (url === "/" || url === "/home") {
+    serveFile(path.join(__dirname, "index.html"), res);
   } else if (url === "/about") {
-    serveFile(path.join(__dirname, "pages", "about.html"), res);
+    serveFile(path.join(__dirname, "about.html"), res);
   } else if (url === "/contact") {
-    serveFile(path.join(__dirname, "pages", "contact.html"), res);
+    serveFile(path.join(__dirname, "contact.html"), res);
   } else if (url === "/style.css") {
     // Serve CSS
     fs.readFile(path.join(__dirname, "public", "style.css"), (err, data) => {
@@ -33,10 +37,19 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   } else {
-    serveFile(path.join(__dirname, "pages", "404.html"), res, 404);
+    serveFile(path.join(__dirname, "404.html"), res, 404);
+  }
+});
+
+server.on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.error(`Port ${PORT} is already in use. Please close the other process or change the port.`);
+  } else {
+    console.error("Server error:", err);
   }
 });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
